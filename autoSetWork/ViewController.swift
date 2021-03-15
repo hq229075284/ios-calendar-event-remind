4f8425516cb7475fea01ad0fe96aa50bb8027aa5
hanqing <229075284@qq.com>
2021-03-16 04:03:07 +0800

    Initial Commit

---
+++ autoSetWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoSetWork
+//
+//  Created by hanqing on 2021/3/16.
+//  Copyright © 2021 hanqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
